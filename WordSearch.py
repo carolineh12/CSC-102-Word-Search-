c1f4dbbd5f5a876a2e469c5abb921a89bf436667
liz-denson <123184723+liz-denson@users.noreply.github.com>
2023-02-16 17:10:25 -0500

    Add files via upload

---
+++ WordSearch.py
@@ -0,0 +1,44 @@
+###############################################################################
+# Word Search
+# Dr. Jean Gourd
+# Last modified on 2020-11-05
+#
+# A template for the main program for the programming assignment Word Search
+#  (part 2).
+# Requires Word.py and Grid.py.
+###############################################################################
+
+# import libraries
+from Word import Word
+from Grid import Grid
+from sys import stdin
+from random import sample, choice
+
+# define constants
+NUM_WORDS = 15              # how many words to randomly select
+GRID_SIZE = 25              # the height/width of the grid
+DISPLAY_SOLUTION = True     # display the solution?
+
+######
+# MAIN
+######
+# read the words from stdin
+    # remove the trailing newline and convert to uppercase
+
+# grab a sampling of the specified number of words
+
+# initialize the grid
+
+# process the words
+    # randomly select an orientation for the current word
+
+    # position the current word at the chosen orientation in the grid
+
+# display stats (i.e., "Successfully placed X of Y words.")
+
+# display the grid
+
+# display the words
+
+# if specified, display the solution
+
